14413daab5062cbca343a4c51898769fe719d3a1
Erna23 <trueyn03@gmail.com>
2025-11-18 19:53:17 +0900

    Add: Week8 실습

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import express from "express";
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleAddStoreToRegion, handleListStoreReviews } from "./controllers/store.controller.js";
@@ -43,10 +45,13 @@ app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형
 app.use(morgan('dev'));       
 app.use(cookieParser());    
 
+/*
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+*/
 
+// 회원가입 API
 app.post("/api/v1/users/signup", handleUserSignUp);
 
 // 1-1 특정 지역에 가게 추가
@@ -96,56 +101,34 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-/*
-const isLogin = (req, res, next) => {
-    // cookie-parser가 만들어준 req.cookies 객체에서 username을 확인
-    const { username } = req.cookies; 
+// Swagger 자동 생성기 설정
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
 
-    if (username) {
-     
-        console.log(`[인증 성공] ${username}님, 환영합니다.`);
-        next(); 
-    } else {
-    
-        console.log('[인증 실패] 로그인이 필요합니다.');
-        res.status(401).send('<script>alert("로그인이 필요합니다!");location.href="/login";</script>');
-    }
-};
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "UMC 9th",
+      description: "UMC 9th Node.js 테스트 프로젝트입니다.",
+    },
+    host: "localhost:3000",
+  };
 
-
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>메인 페이지</h1>
-        <p>이 페이지는 로그인이 필요 없습니다.</p>
-        <ul>
-            <li><a href="/mypage">마이페이지 (로그인 필요)</a></li>
-        </ul>
-    `);
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
 });
-
-
-app.get('/login', (req, res) => {
-    res.send('<h1>로그인 페이지</h1><p>로그인이 필요한 페이지에서 튕겨나오면 여기로 옵니다.</p>');
-});
-
-
-app.get('/mypage', isLogin, (req, res) => {
-    res.send(`
-        <h1>마이페이지</h1>
-        <p>환영합니다, ${req.cookies.username}님!</p>
-        <p>이 페이지는 로그인한 사람만 볼 수 있습니다.</p>
-    `);
-});
-
-
-app.get('/set-login', (req, res) => {
-    res.cookie('username', 'UMC9th', { maxAge: 3600000 });
-    res.send('로그인 쿠키(username=UMC9th) 생성 완료! <a href="/mypage">마이페이지로 이동</a>');
-});
-
-
-app.get('/set-logout', (req, res) => {
-    res.clearCookie('username');
-    res.send('로그아웃 완료 (쿠키 삭제). <a href="/">메인으로</a>');
-});
-*/
